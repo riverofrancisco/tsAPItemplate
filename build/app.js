71432649e190457abc2afbb3444dfe040cf8ed07
riverofrancisco <francisco.rivero124@gmail.com>
2023-06-25 14:37:45 -0300

    App.ts Initial config

--- build/app.js
+++ build/app.js
@@ -1,1 +1,23 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const bodyParser = require("body-parser");
+//Other require routes when exist
+const server = (0, express_1.default)();
+server.use(bodyParser.json());
+server.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+server.use((err, _req, res, _next) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+});
+module.exports = server;
---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from "express";
+const bodyParser = require("body-parser");
+//Other require routes when exist
+
+
+
+const server = express();
+server.use(bodyParser.json());
+server.use((_req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+
+
+server.use((err: any, _req: any, res: any, _next: any) => {
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
+
+
+module.exports = server;
